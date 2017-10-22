635ebf0f61c6bb4efcec879dd528601360661d11
zhouwei <mowuxue1989@163.com>
2017-10-22 16:24:52 +0300

    Src Code

---
+++ config.py
@@ -0,0 +1,48 @@
+'''
+Created on 2017415
+
+@author: zhou
+'''
+import os
+#from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = 'zwzengyy@gmail.com'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN') or 'mowuxue1989@163.com'
+    FLASKY_POSTS_PER_PAGE = 5
+    UPLOAD_FOLDER = os.getcwd() + '\\app\\static\\avatar\\'
+    FLASKY_FOLLOWERS_PER_PAGE = 5
+    FLASKY_COMMENTS_PER_PAGE = 5
+    MAX_SEARCH_RESULTS = 50
+    
+    @staticmethod
+    def init_app(app):
+        pass
+    
+class DevelopmentConfig(Config):
+    DEBUG = True
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'zwzengyy@gmail.com'
+    MAIL_PASSWORD = 'zhouwei1203'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123@localhost/zhouluobo'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+    }
---
+++ manage.py
@@ -0,0 +1,34 @@
+'''
+Created on 2017415
+
+@author: zhou
+'''
+
+import os
+from app import create_app, db
+from app.models import User, Role, Comment, Post, Permission, Follow
+from flask_script import Manager, Shell, Server
+from flask_migrate import Migrate, MigrateCommand
+
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+manager = Manager(app)
+migrate = Migrate(app, db)
+
+def make_shell_context():
+    return dict(app=app,db=db, User=User, Role=Role, Comment=Comment, Post=Post, Permission=Permission, Follow=Follow)
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+manager.add_command("runserver", Server(use_debugger=True))
+
+@manager.command
+def deploy():
+    from flask_migrate import upgrade
+    from app.models import Role, User, Comment, Post, Permission, Follow
+    
+    upgrade()
+    Role.insert_roles()
+    #User.add_self_follows()
+    
+if __name__=='__main__':
+    manager.run()
