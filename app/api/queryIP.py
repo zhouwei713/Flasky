dac100d02fb88ed460c709e7e7f40951d53e1eb8
zhouwei <mowuxue1989@163.com>
2017-11-02 22:44:20 +0300

    make better

--- app/api/queryIP.py
+++ app/api/queryIP.py
@@ -7,7 +7,8 @@ Created on 20171028
 import sys, urllib, urllib2, json
 
 def queryip(ip):
-    url = 'http://api.db-ip.com/v2/134f97b646b750bb40d074c9164620e8c86cbebb/'
+    #url = 'http://api.db-ip.com/v2/134f97b646b750bb40d074c9164620e8c86cbebb/'
+    url = 'https://iptoearth.expeditedaddons.com/?api_key=48GYZWE17301O9CVDX59T6HUSPLAN8FR6I42KMQ32B705J&ip='
     URL = url + ip
     req = urllib2.Request(URL)
     resp = urllib2.urlopen(req)
--- app/api/views.py
+++ app/api/views.py
@@ -20,28 +20,59 @@ def query_ip():
         return redirect(url_for('.ip_detail',ip=ip))
     return render_template('api/query_ip.html',form=form)
 
+# @api.route('/api/ip-detail/<ip>')
+# def ip_detail(ip):
+#     ip = queryip(ip)
+#     countryName = ip['countryName']
+#     countryCode = ip['countryCode']
+#     stateProv = ip['stateProv']
+#     continentName = ip['continentName']
+#     ipAddress = ip['ipAddress']
+#     city = ip['city']
+#     continentCode = ip['continentCode']
+#     location = query_lat_long(countryName,countryCode)
+#     if len(location['locations']) != 0:
+#         lat = location['locations'][0]['latitude']
+#         lon = location['locations'][0]['longitude']
+#         return render_template('api/ip_detail.html', countryName=countryName,
+#                            countryCode=countryCode, stateProv=stateProv,
+#                            continentName=continentName, ipAddress=ipAddress,
+#                            city=city, continentCode=continentCode,
+#                            lat=lat, lon=lon)
+#     return render_template('api/ip_detail.html', countryName=countryName,
+#                                countryCode=countryCode, stateProv=stateProv,
+#                                continentName=continentName, ipAddress=ipAddress,
+#                                city=city, continentCode=continentCode,
+#                            lat=0,lon=0)
+    #return render_template('api/ip_detail.html',ip=ip)
+
 @api.route('/api/ip-detail/<ip>')
 def ip_detail(ip):
     ip = queryip(ip)
-    countryName = ip['countryName']
-    countryCode = ip['countryCode']
-    stateProv = ip['stateProv']
-    continentName = ip['continentName']
-    ipAddress = ip['ipAddress']
-    city = ip['city']
-    continentCode = ip['continentCode']
-    location = query_lat_long(countryName,countryCode)
-    if len(location['locations']) != 0:
-        lat = location['locations'][0]['latitude']
-        lon = location['locations'][0]['longitude']
+    ipAddress = ip['ip']
+    ip_is_available = ip['valid']
+    YIP = request.remote_addr
+    if ip['valid'] == True:
+        countryName = ip['country']
+        countryCode = ip['country_code']
+        region = ip['region']
+        #continentName = ip['continent_code']
+        ipAddress = ip['ip']
+        city = ip['city']
+        continentCode = ip['continent_code']
+        lat = ip['latitude']
+        lon = ip['longitude']
+        # location = query_lat_long(countryName,countryCode)
+        # if len(location['locations']) != 0:
+    #   lat = location['locations'][0]['latitude']
+    #   lon = location['locations'][0]['longitude']
         return render_template('api/ip_detail.html', countryName=countryName,
-                           countryCode=countryCode, stateProv=stateProv,
-                           continentName=continentName, ipAddress=ipAddress,
-                           city=city, continentCode=continentCode,
-                           lat=lat, lon=lon)
-    return render_template('api/ip_detail.html', countryName=countryName,
-                               countryCode=countryCode, stateProv=stateProv,
-                               continentName=continentName, ipAddress=ipAddress,
-                               city=city, continentCode=continentCode,
-                           lat=0,lon=0)
-    #return render_template('api/ip_detail.html',ip=ip)
+                            countryCode=countryCode, region=region,
+                            ipAddress=ipAddress,ip_is_available=ip_is_available,
+                            city=city, continentCode=continentCode,
+                            lat=lat, lon=lon,YIP=YIP)
+    return render_template('api/ip_detail.html', countryName='',
+                               countryCode='', region='',
+                               ipAddress=ipAddress,lat=0, lon=0,
+                               city='', continentCode='',ip_is_available=ip_is_available,
+                           YIP=YIP)
